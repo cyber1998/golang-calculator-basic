848fcbb4a9fcbb274b5b08b1e9656bf972047740
Cyber <cybr1998@gmail.com>
2023-04-12 00:02:25 +0530

    simple functions

---
+++ arithmetic/arithmetic.go
@@ -0,0 +1,17 @@
+package arithmetic
+
+func Add(x int, y int) int {
+	return x + y
+}
+
+func Subtract(x int, y int) int {
+	return x - y
+}
+
+func Multiply(x int, y int) int {
+	return x * y
+}
+
+func Divide(x int, y int) int {
+	return x / y
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"calculator/arithmetic"
+)
+
+func main() {
+	fmt.Println(arithmetic.Multiply(3, 2))
+}
